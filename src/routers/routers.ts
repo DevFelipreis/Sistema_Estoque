ac763aeebda278d1c49268b8a33d29653865311a
DevFelipreis <lfreis.contato@gmail.com>
2024-06-04 18:48:19 -0300

    feat: transpilation js

--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { createUser, deleteUser, getUser, loginAdmin, loginUser, updateUser } from "../controllers/users";
 import { userValidation } from "../middlewares/user";
 import { createProduct, deleteProduct, entryProduct, getProduct, updateProduct } from "../controllers/product";
-import { productValidation, productValidationEntry, productValidationId } from "../middlewares/product";
+import { productValidation, productValidationEntry, productValidationId, productValidationName } from "../middlewares/product";
 import { loginValidation } from "../middlewares/loginvalidation";
 
 const router = Router();
@@ -11,7 +11,7 @@ const router = Router();
 router.post("/login", loginUser);
 router.post("/login/admin", loginAdmin);
 
-router.use(loginValidation);
+//router.use(loginValidation);
 
 router.get("/users", getUser);
 router.post("/users", createUser);
@@ -19,7 +19,7 @@ router.put("/users", userValidation, updateUser)
 router.delete("/users", userValidation, deleteUser)
 
 router.get("/products", getProduct);
-router.post("/products", productValidation, createProduct);
+router.post("/products", productValidation, productValidationName, createProduct);
 router.put("/products", productValidationId, updateProduct);
 router.patch("/products", productValidationId, productValidationEntry, entryProduct);
 router.delete("/products", productValidationId, deleteProduct);
